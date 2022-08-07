35cbdcc4d8a58695a8be261c6c6d2d6ac76e1543
parseb <petra306@protonmail.com>
2022-08-07 21:11:37 +0300

    adds interfaces, minivest, constructor

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.12;
 
-contract Contract {}
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "vest_minimal/MiniVest.sol";
+import "./interfaces/IRegistry.sol";
+import "./interfaces/IDeal.sol";
+
+uint256 constant k = 999999999999999999 * 10 **18;
+
+contract Deal is ERC721("A0Z Deal","DEAL"), MiniVest(k), IDeal {
+
+    IRegistry R;
+    IERC20 Stable;
+
+    constructor(address R_) {
+        R = IRegistry(R_);
+        Stable = IERC20(R.opTokenAddress());
+    }
+
+
+function lastID() public view returns (uint256) {
+    return 11111;
+}
+
+}
---
+++ src/interfaces/IDeal.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.8.0;
+
+
+interface IDeal {
+
+function lastID() external view returns (uint256);
+
+}
