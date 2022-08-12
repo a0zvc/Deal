612bdccb78fc734a3aee0002f1f71822c8a095ac
parseb <petra306@protonmail.com>
2022-08-12 19:36:33 +0300

    removes registry

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/Deal.sol";
-import "Registry/Registry.sol";
 
 contract createDeal is Test {
     function setUp() public {
-        
+
 
     }
 
