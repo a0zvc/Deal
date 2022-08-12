a3760e305156faf808dacfce6377475dcb4a6124
parseb <petra306@protonmail.com>
2022-08-12 17:56:04 +0300

    bf vest_minima reinstall

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Deal.sol";
-import "a0zvc/Registry.sol";
+import "Registry/Registry.sol";
 
 contract createDeal is Test {
     function setUp() public {
+        
 
     }
 
