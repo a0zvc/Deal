14eff988c5077f1df4977289a02b7d446fdbacec
parseb <petra306@protonmail.com>
2022-08-13 22:16:48 +0300

    removes comment

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/Deal.sol";
+//import "Registry/Registry.sol";
+
 
 contract createDeal is Test {
     function setUp() public {
