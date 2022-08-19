2442e206a29aad911785902521bc9c650a87977f
parseb <petra306@protonmail.com>
2022-08-19 14:31:21 +0300

    constant onchain mdai

--- test/createDeal.t.sol
+++ test/createDeal.t.sol
@@ -4,35 +4,35 @@ pragma solidity ^0.8.4;
 import "forge-std/Test.sol";
 import "../src/Deal.sol";
 //import "Registry/IRegistry.sol";
-import "./mocks/mockDAI.sol";
+// import "./mocks/mockDAI.sol";
 
 
 contract createDeal is Test {
 
     IRegistry iR;
-    mockDAI mDAI;
+    IERC20 mDAI;
 
-    address immutable registry_goerli = 0x4baF4Ecdd244807C9303DE843541e4C27edf67AD;
+    address immutable registry_goerli = 0x0E0BD1e019109fddEdb75C00fF8d76075d719788;
     uint immutable MAX_UINT = type(uint).max - 99;
     address deployer = 0xb3F204a5F3dabef6bE51015fD57E307080Db6498;
     address factory = 0xc35DADB65012eC5796536bD9864eD8773aBc74C4; 
     address router = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
-    address opToken;
-
+    address opToken =0xFf6d5B4C418A4064aA9aF61Ac97CeA07d833d3e9;
 
 
     function setUp() public {
+        vm.createSelectFork(vm.envString("GOERLI_ALCHEMY"), 7432492);
         vm.startPrank(deployer);
-        vm.createSelectFork(vm.envString("GOERLI_ALCHEMY"), 7412222);
-
-        iR = IRegistry(registry_goerli);
-        opToken = iR.opTokenAddress();
- 
-        mDAI = opToken == address(0) ? new mockDAI() : mockDAI(iR.opTokenAddress());
-        console.log("OP token is: ",opToken);
-        if (mDAI.allowance(deployer, address(iR)) == 0 ) mDAI.approve(address(iR), MAX_UINT);
-        iR.setExternalPoints(router,factory,address(mDAI), 10, (20*10**18), 1**18);
         
+        iR = IRegistry(registry_goerli);
+        mDAI =  IERC20(opToken); 
+        
+        console.log("OP token is: ",opToken);
+        if (mDAI.allowance(deployer, address(iR)) == 0 )
+        { 
+            mDAI.approve(address(iR), MAX_UINT);
+            iR.setExternalPoints(router,factory,address(mDAI), 10, (20*10**18), 1**18);
+        }
         // if (opToken == address(0)) {
 
         // }
@@ -46,15 +46,16 @@ contract createDeal is Test {
         /// @param _reliableAmt sender approved amount to add as initial liquidity at pool formation
         /// @param _a0zAmount amount to mint and add as initial liquidity at pool formation
         //if (opToken == address(0)) iR.setExternalPoints(router,factory,address(mDAI), 10, (20*10**18), 1**18);
-        opToken = address(mDAI);
+
         vm.stopPrank();
 
     
     }
 
     function testSetupCheck() public {
-        assertTrue(opToken == address(mDAI), "op token is 0");
-        //assertTrue(iR.calculateInitValue() != 0, "initvalue is 0");
+        assertTrue(iR.opTokenAddress() == address(mDAI), "op token is 0");
+        // assertTrue(iR.owner() == deployer, "deployer not owner"); @dev solmate method unavailable
+        assertTrue(iR.calculateInitValue() != 0, "initvalue is 0");
 
 
     }
